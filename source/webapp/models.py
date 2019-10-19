c7518adc1503178f22324b9156c0a5abe44bca2c
Ruslan-dev-1996 <Kasabolotov96.emil.ru>
2019-10-19 12:16:50 +0600

    добавил в моделях поля мигрировал создал фикстуру

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Poll(models.Model):
+    questions = models.TextField(max_length=200, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.questions
+class Choice(models.Model):
+    text = models.TextField(max_length=100, null=False, blank=False, verbose_name='Текст')
+    poll = models.ForeignKey('webapp.Poll', related_name='polls',
+                                on_delete=models.CASCADE, verbose_name='опрос')
